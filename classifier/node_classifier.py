0f5956bbf483fd4c8f42d9fb25d6d1925be23af7
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2025-11-23 20:29:18 -0800

    Staging unsuccessful classifier attempt

--- classifier/node_classifier.py
+++ classifier/node_classifier.py
@@ -1,6 +1,6 @@
 from typing import Literal
 from pathlib import Path
-from models import media_metadata
+from models import media_metadata, path_metadata
 from struct.node import Node
 from extractor.base_extractor import BaseExtractor
 from config.node import NodeType
@@ -37,15 +37,41 @@ class NodeClassifier:
 
         if not node.media_metadata or not node.path_metadata:
             return 'UNKNOWN'
-        if node.path_metadata.format_type == 'SUBTITLE':
+        elif node.path_metadata.format_type == 'SUBTITLE':
             return 'SUBTITLE_FILE'
-        #if node.media_metadata.season or node.media_metadata.episode or 
+        # If contains season or episode pattern and video file
+        elif (
+             (node.media_metadata.season or node.media_metadata.episode or node.media_metadata.season_patterns or node.media_metadata.episode_patterns) and
+             (node.path_metadata.format_type == 'VIDEO')
+             ):
+            return 'EPISODE_FILE'
+        # If contains extras pattern and video file
+        elif node.media_metadata.extras_patterns and node.path_metadata.format_type == 'VIDEO':
+            return 'EXTRAS_FILE'
+        # If video file
+        elif node.path_metadata.format_type == 'VIDEO':
+            return 'MOVIE_FILE'
 
         return 'UNKNOWN'
 
 
     def _classify_dir_contextless(self, node: Node) -> NodeType:
-        return 'UNKNOWN'
+        """
+        Classifies in order of specificity
+        """
+        
+        if not node.media_metadata or not node.path_metadata:
+            return 'UNKNOWN'
+        # TODO add subtitle patterns for SUBTITLE_FOLDER
+        # If contains season or episode pattern
+        elif node.media_metadata.season or node.media_metadata.episode or node.media_metadata.season_patterns or node.media_metadata.episode_patterns:
+            return 'SEASON_FOLDER'
+        # If contains extras pattern and video file
+        elif node.media_metadata.extras_patterns:
+            return 'EXTRAS_FOLDER'
+        # Default to movie folder
+        else:
+            return 'MOVIE_FOLDER'
 
     def _classify_file_contextful(self, node: Node) -> NodeType:
         return 'UNKNOWN'
