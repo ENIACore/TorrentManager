8154bce2ddc5b0a32631e5600ca49f3bd1936fac
Chase Lamkin <github@lamkin.dev>
2025-11-27 08:08:20 +0000

    Modified test_run

--- test_run.py
+++ test_run.py
@@ -1,6 +1,6 @@
 from manager.torrent_manager import TorrentManager
 
 if __name__ == "__main__":
-    manager = TorrentManager(dry_run=True)
+    manager = TorrentManager()
     stats = manager.process_torrents()
     print(f"Results: {stats}")
