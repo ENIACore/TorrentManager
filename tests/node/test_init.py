364998a93ef7c96cbc3cac9bd93968c379c8369e
Chase Lamkin <66813925+ENIACore@users.noreply.github.com>
2025-12-20 23:53:11 -0600

    Modified node tests

--- tests/node/test_init.py
+++ tests/node/test_init.py
@@ -3,12 +3,9 @@ from pathlib import Path
 from unittest.mock import Mock
 import pytest
 
-@pytest.fixture
-def mock_logger(mocker):
-    return mocker.patch('extractor.base_extractor.Logger.get_logger', return_value=Mock())
-
-def test_init(mocker, mock_logger):
+def test_init(mocker):
     mocker.patch('extractor.base_extractor.Logger.get_logger', return_value=Mock())
+
     mocker.patch('tree.node.MediaExtractor.extract_metadata', return_value=Mock())
     mocker.patch('tree.node.PathExtractor.extract_metadata', return_value=Mock())
     node = Node(Path('/test'))
